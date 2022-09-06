8644a0bea506de80a0ef8a872425ee1fd30c6114
Ryan Helsing <ryanhelsing@gmail.com>
2022-09-06 11:51:24 -0400

    init

---
+++ app.py
@@ -0,0 +1,47 @@
+import os
+import base64
+import string
+import random
+
+from flask import Flask
+from flask import request
+from flask import jsonify
+
+import tensorflow as tf
+from basic_pitch.inference import predict
+from basic_pitch import ICASSP_2022_MODEL_PATH
+
+basic_pitch_model = tf.saved_model.load(str(ICASSP_2022_MODEL_PATH))
+
+app = Flask(__name__)
+
+# APP USAGE:
+# flask run --with-threads
+# curl -F 'f=@example.wav' http://127.0.0.1:5000/upload
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+  if request.method == 'POST':
+    f = request.files['f']
+    letters = string.ascii_lowercase
+    file_name = ''.join(random.choice(letters) for i in range(10))
+    p = 'uploads/'+file_name+'.wav'
+    f.save(p)
+    model_output, midi_data, note_activations = predict(
+      p,
+      basic_pitch_model,
+    )
+
+    m = 'saved_midi/'+file_name+'.mid'
+    midi_data.write(m)
+    midi_file = open(m,"rb")
+    midi_data_binary = midi_file.read()
+    midi_data_e = (base64.b64encode(midi_data_binary)).decode('ascii')
+    d = {'midi': midi_data_e}
+
+    if os.path.exists(p):
+      os.remove(p)
+    if os.path.exists(m):
+      os.remove(m)
+
+    return jsonify(d)
